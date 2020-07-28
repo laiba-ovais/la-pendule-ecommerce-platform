80a3527605e73f672c073328992b79e4a1bd05cc
Aun <syed.aun.hashmi.786@gmail.com>
2020-07-28 15:13:09 +0500

    --- Auto-Git Commit ---

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Course from './containers/Course';
+import Payment from './components/Payment/Payment'
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    /<Course /> 
+    {/* /<Course />  */}
+    <Payment/>
   </React.StrictMode>,
   document.getElementById('root')
 );
