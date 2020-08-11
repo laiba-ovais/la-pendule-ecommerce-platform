0b6ac3577e14018ed10e1cb5213427490befa48e
laiba ovais <laibaovais05@gmail.com>
2020-08-11 12:17:25 +0500

    new

--- client/src/containers/CoursesDisplay.js
+++ client/src/containers/CoursesDisplay.js
@@ -4,11 +4,16 @@ import {Link} from 'react-router-dom'
 import {ButtonContainer} from '../components/Button/Button'
 // import Course from './Product';
 import {CourseDetails} from '../components/Course/CourseDetails'
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
 
 export default class CourseDisplay extends Component {
 
     
-  render(props) {
+  render(props){
+    
             const course_id = this.props.location.state;
             console.log(course_id);
             const filteredcourse = CourseDetails.filter(CourseDetails=>{
@@ -17,54 +22,64 @@ export default class CourseDisplay extends Component {
               })
               console.log(filteredcourse);
             
-            return (
               
-                <div className="row">
-                  <div className="col-10 mx-auto text-center text-slanter text-blue my-5">
-                    <h1>
+            return (
+            
+            <Container className="bg-white" >
+                  <div className="col-10 mx-auto text-center  text-blue pt-4 my-5 ss">
+                    <h1 className="font-weight-bolder">
                       {filteredcourse[0].courseTitle}
                     </h1>
                   </div>
                   <div className="row">
-                    <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <div className="ml-5">
+                  <div className=" mx-auto my-3 text-capitalize relative">
                         <img src={filteredcourse[0].courseImage} className="img-fluid" alt="product"/>
-                    </div>
-                    <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                      <h2>Course Title: {filteredcourse[0].courseTitle}</h2>
-                      <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
-                        made by : <span className="text-uppercase">{filteredcourse[0].courseAuthor}</span>
-                      </h4>
-                      <h4 className="text-blue">
-                        <strong>
-                          price: <span>$</span> {filteredcourse[0].price}
-                        </strong>
-                      </h4>
-                      <p className="text-capitalize font-weight-bold mt-3 mb-0">
-                          some info about product:
-                      </p>
-                      <p className="text-muted lead">
-                        {filteredcourse[0].info}
-                      </p>
-                      <div>
+                        </div>
+                        <div className="my-3">
                         <Link to="/">
                           <ButtonContainer>
                             back to Courses
                           </ButtonContainer>
                         </Link>
                           <ButtonContainer 
+                          className="ml-2"
                             // disabled={inCart?true:false}
                             // onClick={()=>{
                             //   value.addToCart(_id);
                             //   value.openModal(_id);
                             // }}
+                            
                           >
                             {filteredcourse[0].inCart ? "inCart":"add to cart"}
                           </ButtonContainer>
+                      
+                    </div>
+                  </div>
+                    
+                    <div className="col-10 mx-auto col-md-8 my-3 text-capitalize inline-block ">
+                      <h3 className="font-weight-bold">Course Title: {filteredcourse[0].courseTitle}</h3>
+                      <h3 className="font-weight-bold text-capitalize mt-3 mb-2">
+                        Instructor : <span className="text-capitalize">{filteredcourse[0].courseAuthor}</span>
+                      </h3>
+                      <h4 className="text-blue">
+                        <strong>
+                          price: <span>$</span> {filteredcourse[0].price}
+                        </strong>
+                      </h4>
+                      <div className="  mt-3 mb-0">
+                        <h3 className="text-capitalize font-weight-bold"> Course Details:</h3>
+                      
+                      <p className="font-weight-bold">
+                        {filteredcourse[0].info}
+                      </p>
                       </div>
                     </div>
                   </div>
-                </div>
-            );
+                
+            </Container>  
+                
+            )
 
           }
 }
