89bee7039ec28c6549f0ca8c13ddb47f0e231666
bisma mansoor <bisma.mansoor@gmail.com>
2020-08-14 02:01:53 +0500

    sduhsd

--- client/src/containers/MainPage/MainPage.js
+++ client/src/containers/MainPage/MainPage.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import './MainPage.css'
 
 export default function MainPage (){
     return(
         <Carousel>
   <Carousel.Item>
+     <div class = "container">
     <img
-      className="d-block min-h-100 w-100"
+      className="d-block w-100"
       src={require("../../static/images/1.jpg")}
       alt="First slide"
     />
+    <button id = "btn" class="btn btn-primary" type="button"> button</button>
+    </div>
     <Carousel.Caption>
       <h3>First slide label</h3>
+      
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100 h-100"
+      className="d-block w-100 "
       src={require("../../static/images/2.jpg")}
       alt="Third slide"
     />
@@ -29,7 +34,7 @@ export default function MainPage (){
   </Carousel.Item>
   <Carousel.Item>
     <img
-      className="d-block w-100 h-100"
+      className="d-block w-100 "
       src={require("../../static/images/3.jpg")}
       alt="Third slide"
     />
