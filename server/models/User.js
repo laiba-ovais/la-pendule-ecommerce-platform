2df50d2a348939f6c3c525b251258af466383edb
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-13 15:07:38 +0500

    --- Auto-Git Commit ---

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../database/database.js')
+
+module.exports = db.sequelize.define(
+  'users',
+  {
+    ID: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: Sequelize.STRING
+    },
+    last_name: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    time: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  },
+  {
+    timestamps: false
+  }
+)
