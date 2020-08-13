d26162f0e06e00fa96a5a2a396f28970a881c6aa
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-13 15:37:45 +0500

    --- Auto-Git Commit ---

--- server/server.js
+++ server/server.js
@@ -17,12 +17,27 @@ const mysqlConnection = mysql.createConnection({
     database: 'mydb'
 });
 mysqlConnection.connect((err) =>{
-    if(!err)
+    if(!err){
     console.log('DB connection successful');
+    var sql = "CREATE TABLE IF NOT EXISTS users (id INT(4),fname VARCHAR(255), lname VARCHAR(255) , email VARCHAR(255) ,password VARCHAR(255))  ";
+    mysqlConnection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });}
     else
     console.log('connection failed \n Error: '+JSON.stringify(err, undefined, 2));
+    
 });
 
+
+// mysqlConnection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");//ISE PRIMARY KEY BANAO
+//     var sql = "CREATE TABLE IF NOT EXISTS users (id INT(4),fname VARCHAR(255), lname VARCHAR(255) , email VARCHAR(255) ,password VARCHAR(255))  ";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table created");
+//   });
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
