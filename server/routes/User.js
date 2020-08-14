6b4ea8e0fefbbd72a769c78a7e11b02bd319eace
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-14 16:23:10 +0500

    --- Auto-Git Commit ---

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,95 +1,68 @@
-const express = require('express')
-const users = express.Router()
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const bcrypt = require('bcrypt');
 
-const User = require('../models/User')
-users.use(cors())
 
-process.env.SECRET_KEY = 'secret'//yeh register wala path  hai kahan?????
-//database connection kahan hai????
-users.post('/register', (req, res) => {
-  const today = new Date()
-  const userData = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    password: req.body.password,
-    date: today
-  }
-<<<<<<< HEAD
-console.log('server k paas jarha hai' + userData.first_name);
-=======
->>>>>>> 20ee67a2d02f2c4b93cf41c1ac33c56e57ccbada
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-    //TODO bcrypt
-    .then(user => {
-      if (!user) {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          userData.password = hash
-          User.create(userData)
-            .then(user => {
-              res.json({ status: user.email + 'Registered!' })
-            })
-            .catch(err => {
-              res.send('error: ' + err)
-            })
-        })
-      } else {
-        res.json({ error: 'User already exists' })
+
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user:'root',
+  password: 'Palkia786',
+  database: 'mydb'
+});
+mysqlConnection.connect((err) =>{
+  if(!err){
+  console.log('DB connection successful');
+  var sql = "CREATE TABLE IF NOT EXISTS users (id INT(4),fname VARCHAR(255), lname VARCHAR(255) , email VARCHAR(255) ,password VARCHAR(255))  ";
+  mysqlConnection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  //   var sql ="INSERT INTO `users` (`ID`, `first_name`,`last_name`, `password`, `email`) VALUES (1, 'test', 'test','test', 'test@test.com');";
+      mysqlConnection.query(sql,function(err,result){if (err) throw err;
+          console.log("values inserted");})
+  });}
+  else
+  console.log('connection failed \n Error: '+JSON.stringify(err, undefined, 2));
+  
+});
+process.on('uncaughtException', function (err) {
+  console.log(err);
+}); 
+
+
+app.post('/submit' , function(req, res){
+      var user = req.body;
+      
+      bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(req.body.password, salt, (err, hash) => {
+            user.password=hash;
+          });
+      });
+      console.log(user.password);
+      
+      var Oneuser={
+          email: user.email,
+          password:user.password,
+          first_name: user.firstname,
+          last_name: user.lastname
       }
-    })
-    .catch(err => {
-      res.send('error: ' + err)
-    })
+      mysqlConnection.query("SELECT * FROM users WHERE email = ?", [Oneuser.email], function(err,rows){
+          if (err) {
+              mysqlConnection.end();
+              console.log(err);
+          }
+          if (!rows.length){
+              mysqlConnection.query("insert into users ( `first_name`,`last_name`, `password`, `email`) values('"+ req.body.first_name +"', '"+req.body.last_name +"', '"+ passwordHashed +"', '"+ req.body.email+"');",function(err2,result){
+                  if(err2)  console.log(err2);
+                  console.log(res.body , "data is saved");
+                  res.json({ status: req.body.email + ' Registered!' }) });
+          }
+          else{
+              console.log("email already exist");
+              res.json("email exist");
+
+          }
 })
 
-users.post('/login', (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-    .then(user => {
-      if (user) {
-        if (bcrypt.compareSync(req.body.password, user.password)) {
-          let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-            expiresIn: 1440
-          })
-          res.send(token)
-        }
-      } else {
-        res.status(400).json({ error: 'User does not exist' })
-      }
-    })
-    .catch(err => {
-      res.status(400).json({ error: err })
-    })
 })
-
-users.get('/profile', (req, res) => {
-  var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
-
-  User.findOne({
-    where: {
-      id: decoded.id
-    }
-  })
-    .then(user => {
-      if (user) {
-        res.json(user)
-      } else {
-        res.send('User does not exist')
-      }
-    })
-    .catch(err => {
-      res.send('error: ' + err)
-    })
-})
-
-module.exports = users
--- server/server.js
+++ server/server.js
@@ -18,63 +18,65 @@ app.use(
 )
 // app.set('view engine', 'handlebars');//// yiya hai pehle wale error ka solution lekin usse doosra error generate hogya
 
-const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password: 'chikorita1191',
-    database: 'mydb'
-});
-mysqlConnection.connect((err) =>{
-    if(!err){
-    console.log('DB connection successful');
-    var sql = "CREATE TABLE IF NOT EXISTS users (id INT(4),fname VARCHAR(255), lname VARCHAR(255) , email VARCHAR(255) ,password VARCHAR(255))  ";
-    mysqlConnection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    //   var sql ="INSERT INTO `users` (`ID`, `first_name`,`last_name`, `password`, `email`) VALUES (1, 'test', 'test','test', 'test@test.com');";
-        mysqlConnection.query(sql,function(err,result){if (err) throw err;
-            console.log("values inserted");})
-    });}
-    else
-    console.log('connection failed \n Error: '+JSON.stringify(err, undefined, 2));
+// const mysqlConnection = mysql.createConnection({
+//     host: 'localhost',
+//     user:'root',
+//     password: 'Palkia786',
+//     database: 'mydb'
+// });
+// mysqlConnection.connect((err) =>{
+//     if(!err){
+//     console.log('DB connection successful');
+//     var sql = "CREATE TABLE IF NOT EXISTS users (id INT(4),fname VARCHAR(255), lname VARCHAR(255) , email VARCHAR(255) ,password VARCHAR(255))  ";
+//     mysqlConnection.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table created");
+//     //   var sql ="INSERT INTO `users` (`ID`, `first_name`,`last_name`, `password`, `email`) VALUES (1, 'test', 'test','test', 'test@test.com');";
+//         mysqlConnection.query(sql,function(err,result){if (err) throw err;
+//             console.log("values inserted");})
+//     });}
+//     else
+//     console.log('connection failed \n Error: '+JSON.stringify(err, undefined, 2));
     
-});
-process.on('uncaughtException', function (err) {
-    console.log(err);
-}); 
-app.post('/submit' , function(req, res){
-        var user = req.body;
-        var passwordHashed = req.body.password;
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(passwordHashed, salt, (err, hash) => {
-                passwordHashed=hash;
-            });
-        });
-        console.log(passwordHashed);
-        
-        var Oneuser={
-            email: user.email,
-            password:user.password,
-            first_name: user.firstname,
-            last_name: user.lastname
-        }
-        mysqlConnection.query("SELECT * FROM users WHERE email = ?", [Oneuser.email], function(err,rows){
-            if (err) {
-                mysqlConnection.end();
-                console.log(err);
-            }
-            if (!rows.length){
-                mysqlConnection.query("insert into users ( `first_name`,`last_name`, `password`, `email`) values('"+ req.body.first_name +"', '"+req.body.last_name +"', '"+ passwordHashed +"', '"+ req.body.email+"');",function(err2,result){
-                    if(err2)  console.log(err2);
-                    console.log(res.body , "data is saved");
-                    res.json({ status: req.body.email + ' Registered!' }) });
-            }
-            else{
-                console.log("email already exist");
-                res.json("email exist");
+// });
+// process.on('uncaughtException', function (err) {
+//     console.log(err);
+// }); 
 
-            }
-})
+
+// app.post('/submit' , function(req, res){
+//         var user = req.body;
+        
+//         bcrypt.genSalt(10, (err, salt) => {
+//             bcrypt.hash(req.body.password, salt, (err, hash) => {
+//               user.password=hash;
+//             });
+//         });
+//         console.log(user.password);
+        
+//         var Oneuser={
+//             email: user.email,
+//             password:user.password,
+//             first_name: user.firstname,
+//             last_name: user.lastname
+//         }
+//         mysqlConnection.query("SELECT * FROM users WHERE email = ?", [Oneuser.email], function(err,rows){
+//             if (err) {
+//                 mysqlConnection.end();
+//                 console.log(err);
+//             }
+//             if (!rows.length){
+//                 mysqlConnection.query("insert into users ( `first_name`,`last_name`, `password`, `email`) values('"+ req.body.first_name +"', '"+req.body.last_name +"', '"+ passwordHashed +"', '"+ req.body.email+"');",function(err2,result){
+//                     if(err2)  console.log(err2);
+//                     console.log(res.body , "data is saved");
+//                     res.json({ status: req.body.email + ' Registered!' }) });
+//             }
+//             else{
+//                 console.log("email already exist");
+//                 res.json("email exist");
+
+//             }
+// })
 
     //     var sql11 = "SELECT email from users where email = '"+req.body.email+"'; ";
     //     var check = mysqlConnection.query (sql11 ,(err)=>
@@ -95,7 +97,7 @@ app.post('/submit' , function(req, res){
     // })
     
     // mysqlConnection.end();
-})
+// })
 
 
 var Users = require('./routes/User')
