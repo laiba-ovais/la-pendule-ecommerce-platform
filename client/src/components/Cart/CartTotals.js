182b830202cec9a7d69b4af8f7be613d34b80722
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-15 23:46:01 +0500

    --- Auto-Git Commit ---

--- client/src/components/Cart/CartTotals.js
+++ client/src/components/Cart/CartTotals.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 // import PayPalButton from './PayPalButton'
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+toast.configure();
 
 export default function CartTotals({value, history}) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
+    const {product} = value; 
+    console.log(product);
+    async function handleToken(token, addresses) {
+        const response = await axios.post("http://localhost:4000/checkout",
+          { token, product }
+        );
+        const { status } = response.data;
+        console.log("Response:", response.data);
+        if (status === "success") {
+          toast("Success! Check email for details", { type: "success" });
+        } else {
+          toast("Something went wrong", { type: "error" });
+        }
+      }
+    
+    
+    
     return (
     <React.Fragment>
         <div className="container">
@@ -35,6 +56,14 @@ export default function CartTotals({value, history}) {
                         </span>
                         <strong>${cartTotal}</strong>
                     </h5>
+                    <StripeCheckout
+                        stripeKey="pk_test_51H3QTXHzmFQ3IEpHb5AQsUscXQoZEINiF2zi8ApZ7gelluyKvuKD2QTSVVYdiSYZWkPYD4up4fuBUcjV7xW8ee4m00eTpjdSaA"
+                        token={handleToken}
+                        amount={cartTotal}
+                        name="Course Payments"
+                        billingAddress
+                        shippingAddress
+                    />
                     {/* <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/> */}
                 </div>
             </div>
---
+++ server/routes/Checkout.js
@@ -0,0 +1,62 @@
+const cors = require("cors");
+const express = require("express");
+const stripe = require("stripe")("sk_test_51H3QTXHzmFQ3IEpH6Rx6NKRr6RH6qBW5LiqW4UCVnYT6eDjcpEfH8i4CLlIWSVPZR2ax0YDyouhnqdyQ9yaO9Vt700G8fEHPhW");
+const uuid = require("uuid/v4");
+const router = express.Router();
+
+
+
+router.use(express.json());
+router.use(cors());
+
+router.get("/", (req, res) => {
+  res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
+});
+
+router.post("/checkout", async (req, res) => {
+  console.log("Request:", req.body);
+
+  let error;
+  let status;
+  try {
+    const { product, token } = req.body;
+
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    });
+
+    const idempotency_key = uuid();
+    const charge = await stripe.charges.create(
+      {
+        amount: product.price * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `Purchased the ${product.name}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip
+          }
+        }
+      },
+      {
+        idempotency_key
+      }
+    );
+    console.log("Charge:", { charge });
+    status = "success";
+  } catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+
+  res.json({ error, status });
+});
+
+module.exports = router;
