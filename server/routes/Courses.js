4e9410b5dfa2f4bad5abf0f7f404972d15641487
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-17 17:07:28 +0500

    --- Auto-Git Commit ---

--- server/routes/Courses.js
+++ server/routes/Courses.js
@@ -14,61 +14,8 @@ router.get('/api/courses',async(req,res)=>{
                  res.send('courses does not exist')
             }
     })
-  // try
-  // {
-  //   let courses = await DB.courses.all();
-  //   res.json(courses);
-  // }
-  // catch(e){
-  //   console.log(e);
-  //   res.sendStatus(500);
-  // }
   
 })
 
-// users.get('/profile', (req, res) => {
-//   var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
-
-//   User.findOne({
-//     where: {
-//       id: decoded.id
-//     }
-//   })
-//     .then(user => {
-//       if (user) {
-//         res.json(user)
-//       } else {
-//         res.send('User does not exist')
-//       }
-//     })
-//     .catch(err => {
-//       res.send('error: ' + err)
-//     })
-// })
-
-// module.exports = users
-
-//export default courses;
 
 module.exports = router;
-// const express = require('express');
-// const router = express.Router();
-
-// // router.get('/api/courses',async(req,res)=>{
-// var mysql = require('mysql');
-
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "Palkia786",
-//   database: "mydb"
-// });
-
-// con.connect(function(err) {
-//   if (err) throw err;
-//   con.query("SELECT * FROM courses", function (err, result, fields) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// });
-
