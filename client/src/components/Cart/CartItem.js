b56eefd47335e4c7aef92f0d9426672e58c6bc51
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-21 18:07:56 +0500

    --- Auto-Git Commit ---

--- client/src/components/Cart/CartItem.js
+++ client/src/components/Cart/CartItem.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 export default function CartItem({ item, value }) {
-  const { _id, courseTitle, courseImage, price , total, count } = item;
+  const { productID, product_name, price , total, count } = item;
   const { increment, decrement, removeItem } = value;
 
   return (
     <div className="row my-2 text-capitalize text-center">
       <div className="col-10 mx-auto col-lg-2">
         <img
-          src={courseImage}
+          src={require(`../../static/images/services/${productID}.png`)}
           style={{ widht: "5rem", height: "5rem" }}
           className="img-fluid"
           alt="product"
@@ -16,7 +16,7 @@ export default function CartItem({ item, value }) {
       </div>
       <div className="col-10 mx-auto col-lg-2">
         <span className="d-lg-none">product:</span>
-        {courseTitle}
+        {product_name}
       </div>
       <div className="col-10 mx-auto col-lg-2">
         <span className="d-lg-none">price :</span>
@@ -26,17 +26,17 @@ export default function CartItem({ item, value }) {
         <div className="d-flex justify-content-center">
           <div>
             <span className="btn btn-black mx-1"
-              onClick={() => decrement(_id)}> -
+              onClick={() => decrement(productID)}> -
             </span>
             <span className="btn btn-black mx-1">{count}</span>
             <span className="btn btn-black mx-1"
-              onClick={() => increment(_id)}> +
+              onClick={() => increment(productID)}> +
             </span>
           </div>
         </div>
       </div>
       <div className="col-10 mx-auto col-lg-2">
-        <div className="cart-icon" onClick={() => removeItem(_id)}>
+        <div className="cart-icon" onClick={() => removeItem(productID)}>
           <i className="fas fa-trash" />
         </div>
       </div>
