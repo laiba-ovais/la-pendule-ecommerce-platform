cfcd3b8306aff76a9e1239ae07bd43b9c8fcb857
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-22 22:03:48 +0500

    Aun

--- server/routes/Checkout.js
+++ server/routes/Checkout.js
@@ -2,7 +2,30 @@ const express = require("express");
 const stripe = require("stripe")("sk_test_51H3QTXHzmFQ3IEpH6Rx6NKRr6RH6qBW5LiqW4UCVnYT6eDjcpEfH8i4CLlIWSVPZR2ax0YDyouhnqdyQ9yaO9Vt700G8fEHPhW");
 const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
+var nodemailer = require('nodemailer');
 
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
 router.get("/", (req, res) => {
   res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
@@ -46,6 +69,29 @@ router.post("/checkout", async (req, res) => {
     );
     console.log("Charge:", { charge });
     status = "success";
+    
+    var transporter = nodemailer.createTransport("SMTP",{
+      service: 'gmail',
+      auth: {
+        user: 'Weprojectnodemailer@gmail.com',
+        pass: 'Palkia786'
+      }
+    });
+
+    var mailOptions = {
+      from: 'syednuhhashmi786@gmail.com',
+      to: token.email,
+      subject: `Payment Success payment id ${token.id}`,
+      text: `Order has been placed of payment id ${token.id} of ${product.name} and will be deilvered at ${charge.address} shortly in 3 days `
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+});
   } catch (error) {
     console.error("Error:", error);
     status = "failure";
