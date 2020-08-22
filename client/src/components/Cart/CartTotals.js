d77150d57af2f6ec1e2ce8f3c82f9c558a373209
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-22 17:39:37 +0500

    --- Auto-Git Commit ---

--- client/src/components/Cart/CartTotals.js
+++ client/src/components/Cart/CartTotals.js
@@ -8,7 +8,9 @@ toast.configure();
 
 export default function CartTotals({value, history}) {
     const {cartSubTotal, cartTax, cartTotal, clearCart} = value;
-    var product = value.product;
+    var product = value.cart.product;
+    var cart = value.cart;
+    var user = value.loggedInUser;
        
 
 
@@ -25,7 +27,39 @@ export default function CartTotals({value, history}) {
           toast("Something went wrong", { type: "error" });
         }
       }
-    
+    //   async function baught() {
+    //     const response = await axios.post("/onbaught",
+    //       {productID:product.productID,
+    //     userId:user.id
+    //     }
+
+    //     );
+    //     const { status } = response.data;
+    //     console.log("Response:", response.data);
+    //     if (status === "success") {
+    //       toast("Success! Check email for details", { type: "success" });
+    //     } else {
+    //       toast("Something went wrong", { type: "error" });
+    //     }
+    //   }
+    const onbaught=(e)=> {
+        e.preventDefaults()
+        cart.product.forEach(element => {
+            axios({  // isse post kr rhy hain email or password thk
+                method: 'POST',
+                url: '/onbaught',
+                data: {
+                  productID    : element.productID,
+                  userid : user.id
+                }}).then(response=>{
+                    console.log(response);
+                } )
+            
+        });
+
+
+        
+    }
     
     
     return (
@@ -58,15 +92,19 @@ export default function CartTotals({value, history}) {
                             total: 
                         </span>
                         <strong>${cartTotal}</strong>
-                    </h5>
+                    </h5>{value.signedin?(
                     <StripeCheckout
-                        stripeKey="pk_test_51H3QTXHzmFQ3IEpHb5AQsUscXQoZEINiF2zi8ApZ7gelluyKvuKD2QTSVVYdiSYZWkPYD4up4fuBUcjV7xW8ee4m00eTpjdSaA"
-                        token={handleToken}
-                        amount={cartTotal}
-                        name="Course Payments"
-                        billingAddress
-                        shippingAddress
-                    />
+                    onClick={onbaught}
+                    stripeKey="pk_test_51H3QTXHzmFQ3IEpHb5AQsUscXQoZEINiF2zi8ApZ7gelluyKvuKD2QTSVVYdiSYZWkPYD4up4fuBUcjV7xW8ee4m00eTpjdSaA"
+                    token={handleToken}
+                    amount={cartTotal}
+                    name="Course Payments"
+                    billingAddress
+                    shippingAddress
+                />
+                  ) : (
+                    <h3>Please sign in for Payments</h3>
+                  )}
                     {/* <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/> */}
                 </div>
             </div>
