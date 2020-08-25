5f05aa9f9351774d2cf7e998ee105f4475c48099
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-25 20:14:43 +0500

    adasd

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 const express = require('express');
 
 const app = express();
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
-  
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 const cors = require("cors");
 app.use(cors());
 
