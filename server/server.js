7ca397a44557c73fb67f5064d03f51d62727fb6e
Aun <syed.aun.hashmi.786@gmail.com>
2020-08-25 20:23:50 +0500

    dfafsa

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const express = require('express');
 
 const app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    res.header("Access-Control-Allow-Origin", "http://peaceful-earth-36070.herokuapp.com.TLD"); // update to match the domain you will make the request from
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'authorization');
+        next();
   });
 const cors = require("cors");
 app.use(cors());
