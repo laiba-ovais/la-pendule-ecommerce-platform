dd56eb3e5b3a0e527017f0ee0764a92ace0b90e0
Aun <syed.aun.hashmi.786@gmail.com>
2020-09-05 22:49:19 +0500

    Aun

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 require('dotenv').config()
 const express = require('express');
-
-const app = express();
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://peaceful-earth-36070.herokuapp.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-app.use(allowCrossDomain);
 const cors = require("cors");
+const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    res.header("Access-Control-Allow-Credentials", true); 
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204);
+    }
+    next();
+  });
+
 app.use(cors());
 
   
