f3ad8bf7d74fc06c04df6c15c22fb1e84aacb584
Aun <syed.aun.hashmi.786@gmail.com>
2020-09-05 22:56:33 +0500

    asa

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require("cors");
 const app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
