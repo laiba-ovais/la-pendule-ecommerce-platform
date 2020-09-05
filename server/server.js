4409284b4c1772752754ac575bf37a2d4897c0e1
Aun <syed.aun.hashmi.786@gmail.com>
2020-09-05 23:58:13 +0500

    aun

--- server/server.js
+++ server/server.js
@@ -2,19 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require("cors");
 const app = express();
-app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header('Access-Control-Allow-Origin:http://peaceful-earth-36070.herokuapp.com')
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-    res.header("Access-Control-Allow-Credentials", true); 
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204);
-    }
-    next();
-  });
+
 
 app.use(cors());
 
